3e5f9a32183d1cddb70cecce5726629b8440b966
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2023-12-28 11:59:53 -0300

    Se definieron los atributos y constructores de la clase Caja

--- src/caja/Caja.java
+++ src/caja/Caja.java
@@ -5,5 +5,19 @@ package caja;
  * @author Santiago Emanuel Castillo
  */
 public class Caja {
+    int ancho;
+    int alto;
+    int profundida;
+    
+    public Caja(){
+        
+    }
+    
+    public Caja(int ancho, int alto, int profundidad){
+        this.ancho = ancho;
+        this.alto = alto;
+        this.profundida = profundidad;
+    }
+    
     
 }
